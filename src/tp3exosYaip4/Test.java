20c74a1339953a963d5fe2d45fe26dd7cba842f8
mjgoat07 <matteo.josuan@etu.univ-amu.fr>
2026-02-12 09:13:58 +0100

    4.2

---
+++ src/tp3exosYaip4/Test.java
@@ -0,0 +1,12 @@
+package tp3exosYaip4;
+
+public class Test {
+    public static void main(String[] args) {
+
+        Student s = new Student("Ali", "Paris", "Informatique", 2, 1500.0);
+        System.out.println(s);
+
+        Staff st = new Staff("Mme Dupont", "Lyon", "Lycée Victor Hugo", 2500.0);
+        System.out.println(st);
+    }
+}
